f9e7031794b3179827b2bd6ad3d79ffdfafafff8
Yousefkataf <159343262+Yousefkataf@users.noreply.github.com>
2024-02-08 00:35:07 -0700

    Replaced the Hello World story with something else

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,11 @@
-print('Hello, World!')
+#Yousef Kataf
+#This program is going to print a short story using the print statements
+
+
+print("As I/m doing this assignment I am struggling with it using the github")
+
+
+print("Now I/m working on it and i think i got it")
+
+
+print('Hopfully this thing works')
